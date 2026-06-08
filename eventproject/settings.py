c4950013f82e2c26e38b15fdb6618737029c9d2d
swayammohanty45 <swayammohanty26@gmail.com>
2026-06-08 14:18:42 +0530

    Fix import os and middleware order

--- eventproject/settings.py
+++ eventproject/settings.py
@@ -1,5 +1,5 @@
-import pymysql
 import os
+import pymysql
 pymysql.install_as_MySQLdb()
 
 from pathlib import Path
@@ -28,13 +28,13 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  ← move here
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'eventproject.urls'
