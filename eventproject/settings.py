0905a86cc4e8729106ab08b6d6ece315a77db084
swayammohanty45 <swayammohanty26@gmail.com>
2026-06-08 14:42:16 +0530

    Switch to PostgreSQL for Render

--- eventproject/settings.py
+++ eventproject/settings.py
@@ -1,17 +1,107 @@
-import os
-import pymysql
-pymysql.install_as_MySQLdb()
+# import os
+# import pymysql
+# pymysql.install_as_MySQLdb()
 
+# from pathlib import Path
+# from datetime import timedelta
+
+# BASE_DIR = Path(__file__).resolve().parent.parent
+# SECRET_KEY = 'django-insecure-eventmaster-2025'
+# DEBUG = False
+# ALLOWED_HOSTS = ['*']
+
+# CSRF_TRUSTED_ORIGINS = [
+#     'https://eventmaster-frontend-alzzfyezx-swayam-mohanty-s-projects.vercel.app',
+# ]
+
+# INSTALLED_APPS = [
+#     'django.contrib.admin',
+#     'django.contrib.auth',
+#     'django.contrib.contenttypes',
+#     'django.contrib.sessions',
+#     'django.contrib.messages',
+#     'django.contrib.staticfiles',
+#     'rest_framework',
+#     'rest_framework_simplejwt',
+#     'corsheaders',
+#     'eventapp',
+# ]
+
+# MIDDLEWARE = [
+#     'corsheaders.middleware.CorsMiddleware',
+#     'django.middleware.security.SecurityMiddleware',
+#     'whitenoise.middleware.WhiteNoiseMiddleware',  ← move here
+#     'django.contrib.sessions.middleware.SessionMiddleware',
+#     'django.middleware.common.CommonMiddleware',
+#     'django.middleware.csrf.CsrfViewMiddleware',
+#     'django.contrib.auth.middleware.AuthenticationMiddleware',
+#     'django.contrib.messages.middleware.MessageMiddleware',
+#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+# ]
+
+# ROOT_URLCONF = 'eventproject.urls'
+
+# TEMPLATES = [{'BACKEND': 'django.template.backends.django.DjangoTemplates',
+#                'DIRS': [], 'APP_DIRS': True,
+#                'OPTIONS': {'context_processors': [
+#                    'django.template.context_processors.request',
+#                    'django.contrib.auth.context_processors.auth',
+#                    'django.contrib.messages.context_processors.messages',
+#                ]}}]
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': os.environ.get('MYSQLDATABASE', 'university'),
+#         'USER': os.environ.get('MYSQLUSER', 'root'),
+#         'PASSWORD': os.environ.get('MYSQLPASSWORD', 'root_db'),
+#         'HOST': os.environ.get('MYSQLHOST', 'localhost'),
+#         'PORT': os.environ.get('MYSQLPORT', '3306'),
+#     }
+# }
+
+# REST_FRAMEWORK = {
+#     'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',),
+#     'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated'],
+# }
+
+# SIMPLE_JWT = {
+#     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+#     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+# }
+
+# CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_CREDENTIALS = True
+
+# LANGUAGE_CODE = 'en-us'
+# TIME_ZONE = 'UTC'
+# USE_I18N = True
+# USE_TZ = True
+
+# STATIC_URL = 'static/'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = BASE_DIR / 'media'
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# WSGI_APPLICATION = 'eventproject.wsgi.application'
+
+
+import os
+import dj_database_url
 from pathlib import Path
 from datetime import timedelta
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = 'django-insecure-eventmaster-2025'
-DEBUG = False
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-eventmaster-2025')
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://eventmaster-frontend-alzzfyezx-swayam-mohanty-s-projects.vercel.app',
+    'https://*.onrender.com',
 ]
 
 INSTALLED_APPS = [
@@ -30,7 +120,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  ← move here
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -49,16 +139,11 @@ TEMPLATES = [{'BACKEND': 'django.template.backends.django.DjangoTemplates',
                    'django.contrib.messages.context_processors.messages',
                ]}}]
 
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('MYSQLDATABASE', 'university'),
-        'USER': os.environ.get('MYSQLUSER', 'root'),
-        'PASSWORD': os.environ.get('MYSQLPASSWORD', 'root_db'),
-        'HOST': os.environ.get('MYSQLHOST', 'localhost'),
-        'PORT': os.environ.get('MYSQLPORT', '3306'),
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 REST_FRAMEWORK = {
@@ -67,8 +152,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ACCESS_TOKEN_LIFETIME': __import__('datetime').timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': __import__('datetime').timedelta(days=7),
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
